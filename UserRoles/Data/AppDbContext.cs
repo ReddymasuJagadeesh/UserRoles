7773c66cd7dec78eb7fc79b3a074da23f93f614a
Jagadeesh <jagadeeshreddimasu@gmail.com>
2025-12-11 18:11:30 +0530

    login employe/admin

---
+++ UserRoles/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using UserRoles.Models;
+namespace UserRoles.Data
+{
+    public class AppDbContext : IdentityDbContext<Users>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected AppDbContext()
+        {
+        }
+    }
+}
---
+++ UserRoles/Services/EmailService.cs
@@ -0,0 +1,37 @@
+﻿
+using Microsoft.AspNetCore.Cors;
+using System.Net;
+using System.Net.Mail;
+
+namespace UserRoles.Services
+{
+    public class EmailService : IEmailService
+    {
+        private readonly IConfiguration _configuration;
+
+        public EmailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task SendEmailAsync(string toEmail, string subject, string body)
+        {
+            var from = _configuration["EmailSettings:From"];
+            var smtpServer = _configuration["EmailSettings:SmtpServer"];
+            var Port = int.Parse(_configuration["EmailSettings:Port"]!);
+            var Username = _configuration["EmailSettings:Username"];
+            var Password = _configuration["EmailSettings:Password"];
+
+            var message = new MailMessage(from!, toEmail, subject, body);
+            var isBodyHtml = true;
+
+            using  var client = new SmtpClient(smtpServer, Port);
+            {
+                client.Credentials = new NetworkCredential(Username, Password);
+                client.EnableSsl = true;    
+                
+            }
+            await client.SendMailAsync(message);
+        }
+    }
+}
